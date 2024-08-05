141c49ffe1b74f741dde89386aaf8d49528d88a5
Diebbo <diego.barbieri03@libero.it>
2024-08-05 15:28:09 +0200

    fixed register schema

--- helpers/schemas.ts
+++ helpers/schemas.ts
@@ -1,19 +1,31 @@
-import { object, ref, string } from "yup";
+import * as yup from "yup";
 
-export const LoginSchema = object().shape({
-  email: string()
+export const LoginSchema = yup.object().shape({
+  email: yup
+    .string()
     .email("This field must be an email")
     .required("Email is required"),
-  password: string().required("Password is required"),
+  password: yup.string().required("Password is required"),
 });
 
-export const RegisterSchema = object().shape({
-  name: string().required("Name is required"),
-  email: string()
+export const RegisterSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .required("Confirm password is required")
+    .oneOf([yup.ref("password")], "Passwords must match"),
+  name: yup.string().required("Name is required"),
+  surname: yup.string().required("Surname is required"),
+  email: yup
+    .string()
     .email("This field must be an email")
     .required("Email is required"),
-  password: string().required("Password is required"),
-  confirmPassword: string()
-    .required("Confirm password is required")
-    .oneOf([ref("password")], "Passwords must match"),
+  birthDate: yup.date().required("Birth date is required").nullable(), // Ensure it can handle Date type correctly
+  phoneNumber: yup.string().optional(),
+  address: yup.string().required("Address is required"),
+  city: yup.string().required("City is required"),
+  state: yup.string().required("State is required"),
+  zip: yup.string().required("ZIP code is required"),
+  country: yup.string().required("Country is required"),
 });
